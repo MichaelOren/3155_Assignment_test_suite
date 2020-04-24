0dc269b105485b91ec4b81d0634932891c50908b
Michael Oren <more0001@student.monash.edu>
2020-04-24 16:49:26 +1000

    Fixed q3 tester

--- tester_q3.py
+++ tester_q3.py
@@ -1,31 +1,28 @@
 import re
 from q3.modified_kmp import modified_kmp
-
 from report_helper import print_diff
+
 
 def tester(text, pattern):
     return [m.start() + 1 for m in re.finditer('(?=' + pattern + ')', text)]
 
 
 if __name__ == "__main__":
-    # text_file = "text_files/reference.txt"
-    # pattern_file = 'text_files/pattern-collection.txt'
-    # with open(text_file) as text_line:
-    #     with open(pattern_file) as patterns:
-    #         text = list(text_line.readlines())[0]
-    #         for i, pattern in enumerate(patterns):
-    #             pattern = pattern.strip()
-    #             works = tes, pattern)
-    #             bm = modified_kmp(text, pattern)
-    #             if not set(works) == set(bm):
-    #                 print(pattern)
-    #                 main_list = len(list(set(works) - set(bm)))
-    #                 # print(sorted(main_list))
-    #                 print(main_list)
-    #                 break
-    #             else:
-    #                 print(i, "True")
+    text_file = "text_files/reference.txt"
+    pattern_file = 'text_files/pattern-collection.txt'
+    with open(text_file) as text_line:
+        with open(pattern_file) as patterns:
+            text = list(text_line.readlines())[0]
+            for i, pattern in enumerate(patterns):
+                pattern = pattern.strip()
+                works = tester(text, pattern)
+                bm = modified_kmp(text, pattern)
+                passed = print_diff(works, bm, pattern, i)
+                if not passed:
+                    break
+            print("====================")
 
+    print("Extra Tests:")
     with open("text_files/add_text_here.txt") as texts:
         with open("text_files/add_patterns_here.txt") as patterns:
             counter = -1
@@ -35,7 +32,7 @@ if __name__ == "__main__":
                 pattern = pattern.strip()
                 works = tester(text, pattern)
                 bm = modified_kmp(text, pattern)
-                passed = print_dff(works, bm, pattern, counter)
+                passed = print_diff(works, bm, pattern, counter)
                 if not passed:
                     break
             print("====================")
