c2a8a18e20cdc5d70d0230d7cd780f6077185af3
Ali Toosi <skho0020@student.monash.edu>
2020-04-24 14:24:07 +1000

    Use report helper for printing the diff

--- tester_q1.py
+++ tester_q1.py
@@ -1,6 +1,8 @@
 import re
 from q1.mirrored_boyermoore import mirrored_boyer_moore
 
+from report_helper import print_diff
+
 def tester(text, pattern):
     return [m.start() + 1 for m in re.finditer('(?=' + pattern + ')', text)]
 
@@ -9,6 +11,7 @@ if __name__ == "__main__":
     text_file = "text_files/reference.txt"
     pattern_file = 'text_files/pattern-collection.txt'
     # pattern_file = 'text_files/pattern-collection_2.txt'
+    print("\n===== First Set =====")
     with open(text_file) as text_line:
         with open(pattern_file) as patterns:
             text = list(text_line.readlines())[0]
@@ -16,27 +19,22 @@ if __name__ == "__main__":
                 pattern = pattern.strip()
                 works = tester(text, pattern)
                 bm = mirrored_boyer_moore(text, pattern)
-                if not set(works) == set(bm):
-                    print(pattern)
-                    main_list = len(list(set(works) - set(bm)))
-                    # print(sorted(main_list))
-                    print(main_list)
+                passed = print_diff(works, bm, pattern, i)
+                if not passed:
                     break
-                else:
-                    print(i, "True")
+            print("======================")
     
+    print("\n===== Second Set ======")
     with open("text_files/add_text_here.txt") as texts:
         with open("text_files/add_patterns_here.txt") as patterns:
+            counter = -1
             for text, pattern in zip(texts, patterns):
+                counter += 1
                 text = text.strip()
                 pattern = pattern.strip()
                 works = tester(text, pattern)
                 bm = mirrored_boyer_moore(text, pattern)
-                if not set(works) == set(bm):
-                    print(pattern)
-                    main_list = len(list(set(works) - set(bm)))
-                    # print(sorted(main_list))
-                    print(main_list)
+                passed = print_diff(works, bm, pattern, counter)
+                if not passed:
                     break
-                else:
-                    print("True")
+            print("=======================")
--- tester_q2.py
+++ tester_q2.py
@@ -1,6 +1,8 @@
 import re
 from q2.wildcard_matching import wildcard
 
+from report_helper import print_diff
+
 def naive_wildcard(text, pattern):
     results = []
     for i in range(len(text) - len(pattern) + 1):
@@ -18,6 +20,7 @@ if __name__ == "__main__":
     text_file = "text_files/reference.txt"
     pattern_file = 'text_files/pattern-collection-wildcards.txt'
     # pattern_file = 'text_files/pattern-collection-wildcards_2.txt'
+    print("\n===== First Set =====")
     with open(text_file) as text_line:
         with open(pattern_file) as patterns:
             text = list(text_line.readlines())[0]
@@ -25,26 +28,20 @@ if __name__ == "__main__":
                 pattern = pattern.strip()
                 works = naive_wildcard(text, pattern)
                 bm = wildcard(text, pattern)
-                if not set(works) == set(bm):
-                    print(pattern)
-                    main_list = list(set(bm) - set(works))
-                    print(sorted(list(main_list)))
-                    print(len(main_list))
-                else:
-                    print(i, "True")
+                print_diff(works, bm, pattern, i)
+            print("====================")
     
+    print("\n===== Second Set =====")
     with open("text_files/add_wildcard_txt_here.txt") as texts:
         with open("text_files/add_wildcard_patterns_here.txt") as patterns:
+            counter = -1
             for text, pattern in zip(texts, patterns):
+                counter += 1
                 text = text.strip()
                 pattern = pattern.strip()
                 works = naive_wildcard(text, pattern)
                 bm = wildcard(text, pattern)
-                if not set(works) == set(bm):
-                    print(pattern)
-                    main_list = len(list(set(works) - set(bm)))
-                    # print(sorted(main_list))
-                    print(main_list)
+                passed = print_diff(works, bm, pattern, counter)
+                if not passed:
                     break
-                else:
-                    print("True")
+            print("=====================")
