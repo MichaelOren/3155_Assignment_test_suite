107b16b3ae974d5678d1dd632df6514ad051be96
Ali Toosi <skho0020@student.monash.edu>
2020-04-24 14:35:03 +1000

    Add total number of occs

--- report_helper.py
+++ report_helper.py
@@ -4,7 +4,7 @@ def print_diff(correct, user, pattern, i):
         print("    Pattern: {}".format(pattern))
         false_trues = list(set(user) - set(correct))
         missed_occs = list(set(correct) - set(user))
-        print("    {} occurences missed and {} occurences mistakenly reported".format(len(missed_occs), len(false_trues)))
+        print("    {} (out of {}) occurences missed and {} occurences mistakenly reported".format(len(missed_occs), len(correct), len(false_trues)))
         return False
     else:
         print("Test #{} passed.".format(i))
